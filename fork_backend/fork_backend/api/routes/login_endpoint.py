b37954cce7a78927aeb4c1b178a00f5fc3fc684e
Jonas Bevernis <jonas.b99@web.de>
2026-02-21 14:02:57 +0100

    improved error handling for login api endpoints

--- fork_backend/fork_backend/api/routes/login_endpoint.py
+++ fork_backend/fork_backend/api/routes/login_endpoint.py
@@ -1,11 +1,16 @@
 """API endpoint for logging in."""
+# pylint: disable=raise-missing-from
+
 from datetime import timedelta
+from sqlalchemy.exc import SQLAlchemyError
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from fork_backend.core.auth import create_access_token, verify_password
+from fork_backend.core.logging import get_logger
 from fork_backend.services.user_service import UserService
 
+log = get_logger()
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/login")
@@ -16,7 +21,22 @@ async def login_for_access_token(
     Uses OAuth2PasswordRequestForm (application/x-www-form-urlencoded).
     """
     service = UserService()
-    user = await service.get_user_by_username(form_data.username)
+    
+    try:
+        user = await service.get_user_by_username(form_data.username)
+    except SQLAlchemyError as sae:
+        log.error(
+            "Failed to get user by username '%s'. Unexpected SQLAlchemyError raised: %s", form_data.username, str(sae))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to authenticate user. Unexpected SQLAlchemyError raised",
+        )
+    except Exception as e:
+        log.error("Failed to get user by username '%s': %s", form_data.username, str(e))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to authenticate user. Unexpected {str(type(e).__name__)} error raised",
+        )
 
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
@@ -25,9 +45,16 @@ async def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    access_token_expires = timedelta(days=7)
-    access_token = create_access_token(
-        data={"sub": user.id}, expires_delta=access_token_expires
-    )
+    try:
+        access_token_expires = timedelta(days=7)
+        access_token = create_access_token(
+            data={"sub": user.id}, expires_delta=access_token_expires
+        )
+    except Exception as e:
+        log.error("Failed to create access token for user '%s': %s", user.id, str(e))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to create access token. Unexpected {str(type(e).__name__)} error raised",
+        )
 
     return {"access_token": access_token, "token_type": "bearer", "user_id": user.id}
