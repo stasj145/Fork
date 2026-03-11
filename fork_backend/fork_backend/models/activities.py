e6c6ae474855a00521c5e2fac933cf374cec4771
Jonas Bevernis <jbe@boreus.de>
2026-03-11 21:16:44 +0100

    added private flag to activity model

--- fork_backend/fork_backend/models/activities.py
+++ fork_backend/fork_backend/models/activities.py
@@ -1,6 +1,6 @@
 """Data model for base activities"""
 from uuid import uuid4
-from sqlalchemy import String, ForeignKey, Date, Float
+from sqlalchemy import String, ForeignKey, Boolean, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from fork_backend.models.base import Base
@@ -15,9 +15,12 @@ class Activities(Base):
     user_id: Mapped[str] = mapped_column(
         ForeignKey("users.id"), nullable=False)
     name: Mapped[str] = mapped_column(String(255), nullable=False, index=True)
-    
-    #amount of calories that are burned per hour and kg of weight
+    private: Mapped[bool] = mapped_column(
+        Boolean, nullable=False, default=False)
+
+    # amount of calories that are burned per hour and kg of weight
     calories_burned_kg_h: Mapped[float] = mapped_column(Float, nullable=False)
 
     # Relationships
-    activity_entries: Mapped[list["ActivityEntry"]] = relationship(back_populates="activity")
+    activity_entries: Mapped[list["ActivityEntry"]
+                             ] = relationship(back_populates="activity")
